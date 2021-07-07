9b70eb91c78a3209091f4af8e8a641d327e88e4e
vitaxa <v.banin@rbkmoney.com>
2021-07-07 15:15:01 +0300

    fix party assertion test

--- src/test/kotlin/com/rbkmoney/porter/PartyListenerTest.kt
+++ src/test/kotlin/com/rbkmoney/porter/PartyListenerTest.kt
@@ -44,7 +44,7 @@ class PartyListenerTest : AbstractIntegrationTest() {
         // Then
         assertEquals(partyChange.partyCreated.id, partyEntity.partyId)
         assertEquals(partyChange.partyCreated.contactInfo.email, partyEntity.email)
-        assertEquals(partyChange.partyCreated.createdAt, TypeUtil.temporalToString(partyEntity.createdAt))
+        assertEquals(partyChange.partyCreated.createdAt, TypeUtil.temporalToString(partyEntity.createdAt?.withNano(0)))
     }
 
     @Test
@@ -72,7 +72,7 @@ class PartyListenerTest : AbstractIntegrationTest() {
         assertTrue(party.status == PartyStatus.blocked)
         assertEquals(partyChange.partyCreated.id, party.partyId)
         assertEquals(partyChange.partyCreated.contactInfo.email, party.email)
-        assertEquals(partyChange.partyCreated.createdAt, TypeUtil.temporalToString(party.createdAt))
+        assertEquals(partyChange.partyCreated.createdAt, TypeUtil.temporalToString(party.createdAt?.withNano(0)))
     }
 
     @Test
@@ -100,6 +100,6 @@ class PartyListenerTest : AbstractIntegrationTest() {
         assertTrue(party.status == PartyStatus.suspended)
         assertEquals(partyChange.partyCreated.id, party.partyId)
         assertEquals(partyChange.partyCreated.contactInfo.email, party.email)
-        assertEquals(partyChange.partyCreated.createdAt, TypeUtil.temporalToString(party.createdAt))
+        assertEquals(partyChange.partyCreated.createdAt, TypeUtil.temporalToString(party.createdAt?.withNano(0)))
     }
 }
